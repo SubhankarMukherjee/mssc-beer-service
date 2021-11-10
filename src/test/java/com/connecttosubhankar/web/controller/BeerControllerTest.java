c0d6076b33a430a3e200a9415e628ed7dab0f751
SubhankarMukherjee <connecttosubhankar@gmail.com>
2021-11-11 03:04:46 +0530

    added beer Controller Test closes #2

---
+++ src/test/java/com/connecttosubhankar/web/controller/BeerControllerTest.java
@@ -0,0 +1,57 @@
+package com.connecttosubhankar.web.controller;
+
+import com.connecttosubhankar.web.model.BeerDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/"+ UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveNewBeer() throws Exception {
+        BeerDto beerDto=BeerDto.builder().beerName("abc").build();
+        String jsonString = objectMapper.writeValueAsString(beerDto);
+        mockMvc.perform(post("/api/v1/beer").content(jsonString).accept(MediaType.APPLICATION_JSON)
+                .header("Content-Type","application/json"))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeer() throws Exception{
+        BeerDto beerDto=BeerDto.builder().build();
+        String jsonString = objectMapper.writeValueAsString(beerDto);
+        mockMvc.perform(put("/api/v1/beer/"+ UUID.randomUUID().toString()).content(jsonString).accept(MediaType.APPLICATION_JSON)
+                .header("Content-Type","application/json"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deleteById() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/"+UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+}
